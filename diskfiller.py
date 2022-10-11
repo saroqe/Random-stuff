7af4873b06a992f70fd91e165217d2d6a19bc81b
Vepsie <114414827+Vepsiee@users.noreply.github.com>
2022-10-11 13:20:56 +0100

    Update diskfiller.py

--- diskfiller.py
+++ diskfiller.py
@@ -1,6 +1,6 @@
 from extras import Payload
 import sys
-
+###And yet it still doesnt work (Tested it on Kali)
 input("This could damage or destroy your computer, use at own risk. This virus can produce 1-10000 self-replicating files. Please undestand i am not responsible for any damages. Would you like to proceed in the execution? (Yes/n)")
 
 if input == "Yes":
